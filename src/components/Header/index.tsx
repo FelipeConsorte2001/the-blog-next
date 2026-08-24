b9dfa4e324ed00c6305fb60439d99e2c134529c7
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-08-24 11:10:35 -0300

    feat: create Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default async function Header() {
+  return (
+    <header>
+      <h1 className='text-4xl/normal font-extrabold py-8 sm:text-5xl/normal sm:py10 md:text-6xl/normal md:py11 lg:text-7xl/normal lg:py12'>
+        <Link href='#'>The Blog</Link>
+      </h1>
+    </header>
+  );
+}
