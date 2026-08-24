acd39f039d17282855d057b6ccc5c721de7bffe6
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-08-24 11:09:39 -0300

    feat: create json repository

---
+++ src/repositories/post/json-post-repository.ts
@@ -0,0 +1,36 @@
+import { PostModel } from '@/models/post/post-model';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+import { PostRepository } from './post-repository';
+
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE = resolve(ROOT_DIR, 'src', 'db', 'seed', 'posts.json');
+const SIMULATE_WAIT_IN_MS = 0;
+export class JsonPostRepository implements PostRepository {
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE, 'utf8');
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
+
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
+    await new Promise(resolve => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+    return await this.readFromDisk();
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    await this.simulateWait();
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+
+    if (!post) throw new Error('Post não encontrado');
+
+    return post;
+  }
+}
