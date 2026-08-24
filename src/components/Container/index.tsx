a9c2b0d368ecd1a7a928239f59b91d17fbf361e1
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-08-24 11:10:47 -0300

    feat: create Container Component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,10 @@
+type ContainerProps = {
+  children: React.ReactNode;
+};
+export default async function Container({ children }: ContainerProps) {
+  return (
+    <div className='text-slate-900 bg-slate-100 min-h-screen'>
+      <div className='max-w-5xl mx-auto px-8'>{children}</div>;
+    </div>
+  );
+}
