e733ecc530a492a24b857616610354062bd26b3f
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-09-09 12:38:16 -0300

    fix remove unsuable repository

--- src/db/drizzle/seed.ts
+++ src/db/drizzle/seed.ts
@@ -1,16 +1,26 @@
-import { JsonPostRepository } from '@/repositories/post/json-post-repository';
+import { PostModel } from '@/models/post/post-model';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
 import { drizzleDb } from '.';
 import { postsTable } from './schemas';
 
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE = resolve(ROOT_DIR, 'src', 'db', 'seed', 'posts.json');
+
 (async () => {
-  const jsonPostRepository = new JsonPostRepository();
-  const posts = await jsonPostRepository.findAll();
+  const jsonContent = await readFile(JSON_POSTS_FILE, 'utf8');
+  const parsedJson = JSON.parse(jsonContent);
+  const { posts } = parsedJson;
+
+  const postFiltered = posts?.filter(
+    (post: PostModel) => post.published === true,
+  );
 
   try {
     await drizzleDb.delete(postsTable);
-    await drizzleDb.insert(postsTable).values(posts);
+    await drizzleDb.insert(postsTable).values(postFiltered);
 
-    console.log(`${posts.length} posts foram salvos na base de dados.`);
+    console.log(`${postFiltered.length} posts foram salvos na base de dados.`);
   } catch (e) {
     console.log('Ocorreu um erro...');
     console.log(e);
